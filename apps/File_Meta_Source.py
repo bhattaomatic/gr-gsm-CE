b244f17d86c78797e070d36883a44f588b1abcdc
bhattaomatic <bhattaomatic@gmail.com>
2017-08-30 00:17:15 +0200

    strictural changes

--- apps/File_Meta_Source.py
+++ apps/File_Meta_Source.py
@@ -3,57 +3,22 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: File Meta Source
-# Generated: Tue Aug 29 14:58:43 2017
+# Generated: Tue Aug 29 23:46:00 2017
 ##################################################
 
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print "Warning: failed to XInitThreads()"
-
-from PyQt4 import Qt
-from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import gr
-from gnuradio import qtgui
 from gnuradio import zeromq
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
 import SVM
-import sip
-import sys
 
 
-class File_Meta_Source(gr.top_block, Qt.QWidget):
+class File_Meta_Source(gr.top_block):
 
     def __init__(self):
         gr.top_block.__init__(self, "File Meta Source")
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("File Meta Source")
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("GNU Radio", "File_Meta_Source")
-        self.restoreGeometry(self.settings.value("geometry").toByteArray())
 
         ##################################################
         # Variables
@@ -64,75 +29,14 @@ class File_Meta_Source(gr.top_block, Qt.QWidget):
         # Blocks
         ##################################################
         self.zeromq_sub_msg_source_0 = zeromq.sub_msg_source("tcp://192.168.42.2:4729", 100000)
-        self.qtgui_time_sink_x_0_0 = qtgui.time_sink_f(
-        	48*2, #size
-        	1e6, #samp_rate
-        	"Rx Frame from file", #name
-        	1 #number of inputs
-        )
-        self.qtgui_time_sink_x_0_0.set_update_time(1)
-        self.qtgui_time_sink_x_0_0.set_y_axis(-1, 1)
-        
-        self.qtgui_time_sink_x_0_0.set_y_label("Amplitude", "")
-        
-        self.qtgui_time_sink_x_0_0.enable_tags(-1, True)
-        self.qtgui_time_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0_0.enable_autoscale(True)
-        self.qtgui_time_sink_x_0_0.enable_grid(True)
-        self.qtgui_time_sink_x_0_0.enable_control_panel(False)
-        
-        if not True:
-          self.qtgui_time_sink_x_0_0.disable_legend()
-        
-        labels = ["", "", "", "", "",
-                  "", "", "", "", ""]
-        widths = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-                  "magenta", "yellow", "dark red", "dark green", "blue"]
-        styles = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
-        markers = [-1, -1, -1, -1, -1,
-                   -1, -1, -1, -1, -1]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-        
-        for i in xrange(1):
-            if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_time_sink_x_0_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0_0.set_line_alpha(i, alphas[i])
-        
-        self._qtgui_time_sink_x_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0.pyqwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_0_win)
-        self.blocks_tagged_stream_to_pdu_0 = blocks.tagged_stream_to_pdu(blocks.float_t, "packet_len")
-        self.blocks_pdu_to_tagged_stream_0_0 = blocks.pdu_to_tagged_stream(blocks.complex_t, "packet_len")
-        self.blocks_complex_to_mag_0_0 = blocks.complex_to_mag(1)
-        self.SVM_print_msg_0_0 = SVM.print_msg("meta")
-        self.SVM_print_msg_0 = SVM.print_msg("data")
+        self.SVM_prediction_printer_0 = SVM.prediction_printer("No Person", "Person Walking", 50, 0.5)
         self.SVM_Test_0 = SVM.Test()
 
         ##################################################
         # Connections
         ##################################################
-        self.msg_connect((self.SVM_Test_0, 'out'), (self.SVM_print_msg_0, 'in'))    
-        self.msg_connect((self.SVM_Test_0, 'timestamp'), (self.SVM_print_msg_0_0, 'in'))    
-        self.msg_connect((self.blocks_tagged_stream_to_pdu_0, 'pdus'), (self.SVM_Test_0, 'data'))    
-        self.msg_connect((self.zeromq_sub_msg_source_0, 'out'), (self.blocks_pdu_to_tagged_stream_0_0, 'pdus'))    
-        self.connect((self.blocks_complex_to_mag_0_0, 0), (self.blocks_tagged_stream_to_pdu_0, 0))    
-        self.connect((self.blocks_complex_to_mag_0_0, 0), (self.qtgui_time_sink_x_0_0, 0))    
-        self.connect((self.blocks_pdu_to_tagged_stream_0_0, 0), (self.blocks_complex_to_mag_0_0, 0))    
-
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "File_Meta_Source")
-        self.settings.setValue("geometry", self.saveGeometry())
-        event.accept()
-
+        self.msg_connect((self.SVM_Test_0, 'out'), (self.SVM_prediction_printer_0, 'in'))    
+        self.msg_connect((self.zeromq_sub_msg_source_0, 'out'), (self.SVM_Test_0, 'data'))    
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -143,21 +47,9 @@ class File_Meta_Source(gr.top_block, Qt.QWidget):
 
 def main(top_block_cls=File_Meta_Source, options=None):
 
-    from distutils.version import StrictVersion
-    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
-    qapp = Qt.QApplication(sys.argv)
-
     tb = top_block_cls()
     tb.start()
-    tb.show()
-
-    def quitting():
-        tb.stop()
-        tb.wait()
-    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
-    qapp.exec_()
+    tb.wait()
 
 
 if __name__ == '__main__':
